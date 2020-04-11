2184eb33d616b87d7e2f6303ce4e34adba55b327
Chans321 <tsschand@gmail.com>
2020-04-11 17:38:26 +0530

    flask app fixed

--- flask app/app.py
+++ flask app/app.py
@@ -1,11 +1,17 @@
 from flask import Flask           
 from flask import render_template
 from getLatestNews import send_news
-app = Flask(__name__)            
+app = Flask(__name__)
+import time            
 
-@app.route("/")                  
-def hello():
-    titles,news=send_news()                     
+
+titles=[] 
+
+news=[]
+@app.route("/")
+def hello():                 
     return  render_template('index.html',titles=titles, news=news)
-if __name__ == "__main__":        
+if __name__ == "__main__":
+    titles,news=send_news()  
+    time.sleep(9.4)        
     app.run()                 
